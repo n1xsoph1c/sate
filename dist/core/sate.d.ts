8e450a33af8b7627011f7a123bc486da3cc41a05
n1xsoph1c <pttrafiahmed@gmail.com>
2023-09-29 10:44:31 +0600

    Updated Bug

--- dist/core/sate.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import { TencodedPage } from "./types";
-export declare class SATE {
-    private encoder;
-    private decoder;
-    encode(text: string): TencodedPage[];
-    decode(data: TencodedPage[]): import("react").ReactNode;
-}
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { SATE } from "./core/sate";
-declare const _default: {
-    SATE: typeof SATE;
-};
-export default _default;
